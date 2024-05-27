bf26bcf129eab29db3aaad2fc8f575cd0eb211fd
radziu2402 <radziu2402@gmail.com>
2024-05-27 11:27:17 +0200

    Eliminate collisions synchronizing

--- src/employee/Employee.cpp
+++ src/employee/Employee.cpp
@@ -10,27 +10,6 @@
 #include "../config/Utilities.h"
 
 std::mutex mx_positions;
-std::map<std::string, bool> occupiedPositions;
-
-std::string makeKey(int x, int y) {
-    return std::to_string(x) + "," + std::to_string(y);
-}
-
-bool isPositionFree(int x, int y) {
-    std::lock_guard <std::mutex> lock(mx_positions);
-    auto key = makeKey(x, y);
-    return occupiedPositions.find(key) == occupiedPositions.end();
-}
-
-void occupyPosition(int x, int y) {
-    std::lock_guard <std::mutex> lock(mx_positions);
-    occupiedPositions[makeKey(x, y)] = true;
-}
-
-void freePosition(int x, int y) {
-    std::lock_guard <std::mutex> lock(mx_positions);
-    occupiedPositions.erase(makeKey(x, y));
-}
 
 void startEmployeeSimulation() {
     std::srand(std::time(nullptr) + std::hash < std::thread::id > {}(std::this_thread::get_id()));
@@ -51,19 +30,9 @@ void startEmployeeSimulation() {
     WINDOW *employee_window = newwin(employee_height, employee_width, employee_start_y, employee_start_x);
     wattron(employee_window, COLOR_PAIR(color_pair_number));
 
-    occupyPosition(employee_start_x, employee_start_y);
-
     while (program_running.load()) {
-        bool shouldMove = false;
-
-        if (employee_start_x < waiting_for_elevator_x && isPositionFree(employee_start_x + 1, employee_start_y)) {
-            occupyPosition(employee_start_x + 1, employee_start_y);
-            freePosition(employee_start_x, employee_start_y);
+        if (employee_start_x < waiting_for_elevator_x){
             ++employee_start_x;
-            shouldMove = true;
-        }
-
-        if (shouldMove) {
             std::lock_guard <std::mutex> guard(mx_drawing);
             werase(employee_window);
             mvwin(employee_window, employee_start_y, employee_start_x);
@@ -86,7 +55,6 @@ void startEmployeeSimulation() {
         std::lock_guard<std::mutex> guard(mx_drawing);
         if (passengers.size() < 18) {
             passengers.push_back(newPassenger);
-            freePosition(employee_start_x, employee_start_y);
             werase(employee_window);
             wrefresh(employee_window);
             delwin(employee_window);
@@ -105,19 +73,10 @@ void startEmployeeSimulation() {
 
     WINDOW *employee_window_after_exit = newwin(employee_height, employee_width, our_exit_floor + 1, employee_start_x_after_exit);
     wattron(employee_window_after_exit, COLOR_PAIR(color_pair_number));
-    occupyPosition(exit_floor + 1, employee_start_x_after_exit);
 
     while (program_running.load()) {
-        bool shouldMove = false;
-        if (exit_floor < waiting_for_elevator_x &&
-            isPositionFree(employee_start_x_after_exit + 1, our_exit_floor + 1)) {
-            occupyPosition(employee_start_x_after_exit + 1, our_exit_floor + 1);
-            freePosition(employee_start_x_after_exit, our_exit_floor + 1);
+        if (exit_floor < waiting_for_elevator_x) {
             ++employee_start_x_after_exit;
-            shouldMove = true;
-        }
-
-        if (shouldMove) {
             std::lock_guard <std::mutex> guard(mx_drawing);
             werase(employee_window_after_exit);
             mvwin(employee_window_after_exit, our_exit_floor + 1, employee_start_x_after_exit + 1);
@@ -133,7 +92,6 @@ void startEmployeeSimulation() {
     std::this_thread::sleep_for(std::chrono::seconds(3));
     {
         std::lock_guard<std::mutex> guard(mx_drawing);
-        freePosition(employee_start_x_after_exit, our_exit_floor + 1);
         werase(employee_window_after_exit);
         wrefresh(employee_window_after_exit);
         delwin(employee_window_after_exit);
